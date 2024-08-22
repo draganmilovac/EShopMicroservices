d0824c36fa3053dcf5e7369ef491ee40742284dc
AzureAD\DraganMilovac <dragan.milovac@unifiedpost.com>
2024-08-22 12:37:04 +0200

    Start with minimal api

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductCommnadHandler.cs
@@ -0,0 +1,26 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommnad(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductCommnadHandler 
+        : ICommandHandler<CreateProductCommnad, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommnad command, CancellationToken cancellationToken)
+        {
+            var product = new Product()
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price,
+            };
+
+            return new CreateProductResult(Guid.NewGuid());
+        }
+    }
+}
