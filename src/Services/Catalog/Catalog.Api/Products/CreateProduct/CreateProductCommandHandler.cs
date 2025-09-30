c5318a8e2e84d76ccdf40a17fc51517c307d5ed9
AzureAD\DraganMilovac <dragan.milovac@unifiedpost.com>
2025-09-30 15:34:40 +0200

    Create building blocks and put common things to one project

---
+++ src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.Api.Models;
+using MediatR;
+
+namespace Catalog.Api.Products.CreateProduct
+{
+    internal record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+        : ICommand<CreateProductResult>;
+    internal record CreateProductResult(Guid Id);
+
+
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            var product = new Product()
+            {
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price,
+                Name = command.Name,
+            };
+
+            return new CreateProductResult(Guid.NewGuid());
+        }
+    }
+}
