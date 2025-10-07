f94632c2055c19e37fd4df1d452d6fd73a68938d
AzureAD\DraganMilovac <dragan.milovac@unifiedpost.com>
2025-10-07 16:07:35 +0200

    Create GetProducts endpoint and initialize docker compose

--- src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductCommandHandler.cs
+++ src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductCommandHandler.cs
@@ -1,15 +1,15 @@
 ﻿using BuildingBlocks.CQRS;
 using Catalog.Api.Models;
-using MediatR;
+using Marten;
 
 namespace Catalog.Api.Products.CreateProduct
 {
-    internal record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
         : ICommand<CreateProductResult>;
-    internal record CreateProductResult(Guid Id);
+    public record CreateProductResult(Guid Id);
 
 
-    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductCommandHandler (IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
@@ -22,7 +22,10 @@ namespace Catalog.Api.Products.CreateProduct
                 Name = command.Name,
             };
 
-            return new CreateProductResult(Guid.NewGuid());
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new CreateProductResult(product.Id);
         }
     }
 }
--- src/Services/Catalog/Catalog.Api/Program.cs
+++ src/Services/Catalog/Catalog.Api/Program.cs
@@ -1,5 +1,22 @@
+using Carter;
+using Marten;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+})
+.UseLightweightSessions();
 
 var app = builder.Build();
 
+app.MapCarter();
 app.Run();
